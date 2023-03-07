a6e7f8f50a1b7521b6f52b287c6cc7cbc6b1a21b
Chrys Rakotonimanana <chrysrakk@gmail.com>
2023-03-07 18:49:06 +0300

    parsing args

--- lib/args-parser.ts
+++ lib/args-parser.ts
@@ -1,21 +1,36 @@
 
+type TArgs = { [key: string]: string | boolean | number | undefined };
 export default class ArgsParser {
 
-    public args: { [key: string]: string } =  {};
+    public args: TArgs =  {};
     constructor() {
         this.parseArgs(Deno.args);
     }
 
-    parseArgs(args: string[]) {
-        const parsedArgs: { [key: string]: string } = {};
-        for (let i = 0; i < args.length; i++) {
+    private parseArgs(args: string[]) {
+        const parsedArgs:TArgs = {};
+        for (let i = 0; i < args.length; i+=2) {
             const arg = args[i];
-            if (arg.startsWith("--")) {
-                const [key, value] = arg.split("=");
-                parsedArgs[key] = value;
+            if (arg === "--help") {
+                parsedArgs[this.formatArgs(arg)] = true;
+                break;
+            } else if (arg.startsWith("--") || arg.startsWith("-")) {
+                parsedArgs[this.formatArgs(arg)] = args[i+1];
             }
         }
         this.args = parsedArgs;
+    }
+
+    formatArgs(key: string): string {
+        return key.replace(/^-*/, "");
+    }
+
+    isEmpty() {
+        return Object.keys(this.args).length === 0;
+    }
+
+    help() {
+        return `Usage: calendar [OPTION]... [MONTH] [YEAR]`;
     }
 
     get argKeys() {
