7c4d35da4da535d43ceb3fa9cb3e81d233205f32
Chrys Rakotonimanana <chrysrakk@gmail.com>
2023-03-07 18:49:16 +0300

    file for CLI

---
+++ calendar.ts
@@ -0,0 +1,16 @@
+// This is a CLI tool to generate a calendar for a given month and year.
+import Calendar from "./index.ts";
+import ArgsParser from "./lib/args-parser.ts";
+
+const parser = new ArgsParser();
+
+if (parser.isEmpty()) {
+    console.log("No arguments provided. See --help for more information.");
+    Deno.exit(1);
+}
+console.log(parser.args);
+
+if ("help" in parser.args) {
+    console.log(parser.help());
+    Deno.exit(0);
+}
